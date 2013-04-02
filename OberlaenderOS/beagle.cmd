06d2b4f3d7b4a12a249050b6da3a5c1ef2292986
Danielku15 <danielku15@coderline.net>
2013-04-02 19:13:50 +0200

    bootloader cleanup, booting to system mode

--- OberlaenderOS/beagle.cmd
+++ OberlaenderOS/beagle.cmd
@@ -10,13 +10,11 @@
 MEMORY
 {
    int_ram:  ORIGIN = 0x40200000  LENGTH = 0x0000FFFF
-   ext_ddr:  ORIGIN = 0x82000000  LENGTH = 0x00010000
+   ext_ddr:  ORIGIN = 0x82000000  LENGTH = 0x10000000
 }
 
 SECTIONS
 {
-
-
    .const      > ext_ddr
    .bss        > ext_ddr
    .far        > ext_ddr
@@ -29,4 +27,14 @@ SECTIONS
    .text       > ext_ddr
    .sysmem     > ext_ddr
    .switch     > ext_ddr
+
+   .stackarea  > ext_ddr {
+		/* 4k stacks */
+		. = . + (4 * 1024);
+		kernelStack = .;
+		. = . + (4 * 1024);
+		irqStack = .;
+		. = . + (4 * 1024);
+		systemStack = .;
+   }
 }
